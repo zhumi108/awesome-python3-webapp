63fb042f8c6aac1a922a4f1c5364374bd325717a
Jeremy <1034157933@qq.com>
2017-06-22 20:29:43 +0800

    基本功能完成

--- www/apis.py
+++ www/apis.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__author__ = 'Michael Liao'
+
 '''
-JSON API definition
+JSON API definition.
 '''
 
 import json, logging, inspect, functools
 
 class Page(object):
+    '''
+    Page object for display pages.
+    '''
 
     def __init__(self, item_count, page_index=1, page_size=10):
+        '''
+        Init Pagination by item_count, page_index and page_size.
+
+        >>> p1 = Page(100, 1)
+        >>> p1.page_count
+        10
+        >>> p1.offset
+        0
+        >>> p1.limit
+        10
+        >>> p2 = Page(90, 9, 10)
+        >>> p2.page_count
+        9
+        >>> p2.offset
+        80
+        >>> p2.limit
+        10
+        >>> p3 = Page(91, 10, 10)
+        >>> p3.page_count
+        10
+        >>> p3.offset
+        90
+        >>> p3.limit
+        10
+        '''
         self.item_count = item_count
         self.page_size = page_size
         self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
@@ -30,20 +60,36 @@ class Page(object):
     __repr__ = __str__
 
 class APIError(Exception):
+    '''
+    the base APIError which contains error(required), data(optional) and message(optional).
+    '''
     def __init__(self, error, data='', message=''):
         super(APIError, self).__init__(message)
-        self.error=error
-        self.data=data
-        self.message=message
+        self.error = error
+        self.data = data
+        self.message = message
 
 class APIValueError(APIError):
+    '''
+    Indicate the input value has error or invalid. The data specifies the error field of input form.
+    '''
     def __init__(self, field, message=''):
         super(APIValueError, self).__init__('value:invalid', field, message)
 
 class APIResourceNotFoundError(APIError):
+    '''
+    Indicate the resource was not found. The data specifies the resource name.
+    '''
     def __init__(self, field, message=''):
         super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
 
 class APIPermissionError(APIError):
+    '''
+    Indicate the api has no permission.
+    '''
     def __init__(self, message=''):
-        super(APIPermissionError, self).__init__('permission:forbidder', 'permission', message)
+        super(APIPermissionError, self).__init__('permission:forbidden', 'permission', message)
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
--- www/handlers.py
+++ www/handlers.py
@@ -40,7 +40,7 @@ def user2cookie(user, max_age):
     '''
     # build cookie string by: id-expires-sha1
     expires = str(int(time.time() + max_age))
-    s = '%s-%s-%s-%s' % (user.id, user.password, expires, _COOKIE_KEY)
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
 
@@ -49,7 +49,7 @@ def text2html(text):
     return ''.join(lines)
 
 @asyncio.coroutine
-async def cookie2user(cookie_str):
+def cookie2user(cookie_str):
     '''
     Parse cookie and load user if cookie is valid.
     '''
@@ -62,28 +62,28 @@ async def cookie2user(cookie_str):
         uid, expires, sha1 = L
         if int(expires) < time.time():
             return None
-        user = await User.find(uid)
+        user = yield from User.find(uid)
         if user is None:
             return None
-        s = '%s-%s-%s-%s' % (uid, user.password, expires, _COOKIE_KEY)
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
         if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
             logging.info('invalid sha1')
             return None
-        user.password = '******'
+        user.passwd = '******'
         return user
     except Exception as e:
         logging.exception(e)
         return None
 
 @get('/')
-async def index(*, page='1'):
+def index(*, page='1'):
     page_index = get_page_index(page)
-    num = await Blog.findNumber('count(id)')
+    num = yield from Blog.findNumber('count(id)')
     page = Page(num)
     if num == 0:
         blogs = []
     else:
-        blogs = await Blog.findAll(orderBy='created_at desc', limit=(page.offset, page.limit))
+        blogs = yield from Blog.findAll(orderBy='created_at desc', limit=(page.offset, page.limit))
     return {
         '__template__': 'blogs.html',
         'page': page,
@@ -91,9 +91,9 @@ async def index(*, page='1'):
     }
 
 @get('/blog/{id}')
-async def get_blog(id):
-    blog = await Blog.find(id)
-    comments = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
+def get_blog(id):
+    blog = yield from Blog.find(id)
+    comments = yield from Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
     for c in comments:
         c.html_content = text2html(c.content)
     blog.html_content = markdown2.markdown(blog.content)
@@ -115,13 +115,14 @@ def signin():
         '__template__': 'signin.html'
     }
 
+# user login 
 @post('/api/authenticate')
-async def authenticate(*, email, passwd):
+def authenticate(*, email, passwd):
     if not email:
         raise APIValueError('email', 'Invalid email.')
     if not passwd:
         raise APIValueError('passwd', 'Invalid password.')
-    users = await User.findAll('email=?', [email])
+    users = yield from User.findAll('email=?', [email])
     if len(users) == 0:
         raise APIValueError('email', 'Email not exist.')
     user = users[0]
@@ -135,7 +136,7 @@ async def authenticate(*, email, passwd):
     # authenticate ok, set cookie:
     r = web.Response()
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
-    user.password = '******'
+    user.passwd = '******'
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
@@ -190,93 +191,93 @@ def manage_users(*, page='1'):
     }
 
 @get('/api/comments')
-async def api_comments(*, page='1'):
+def api_comments(*, page='1'):
     page_index = get_page_index(page)
-    num = await Comment.findNumber('count(id)')
+    num = yield from Comment.findNumber('count(id)')
     p = Page(num, page_index)
     if num == 0:
         return dict(page=p, comments=())
-    comments = await Comment.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    comments = yield from Comment.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
     return dict(page=p, comments=comments)
 
 @post('/api/blogs/{id}/comments')
-async def api_create_comment(id, request, *, content):
+def api_create_comment(id, request, *, content):
     user = request.__user__
     if user is None:
         raise APIPermissionError('Please signin first.')
     if not content or not content.strip():
         raise APIValueError('content')
-    blog = await Blog.find(id)
+    blog = yield from Blog.find(id)
     if blog is None:
         raise APIResourceNotFoundError('Blog')
     comment = Comment(blog_id=blog.id, user_id=user.id, user_name=user.name, user_image=user.image, content=content.strip())
-    await comment.save()
+    yield from comment.save()
     return comment
 
 @post('/api/comments/{id}/delete')
-async def api_delete_comments(id, request):
+def api_delete_comments(id, request):
     check_admin(request)
-    c = await Comment.find(id)
+    c = yield from Comment.find(id)
     if c is None:
         raise APIResourceNotFoundError('Comment')
-    await c.remove()
+    yield from c.remove()
     return dict(id=id)
 
 @get('/api/users')
-async def api_get_users(*, page='1'):
+def api_get_users(*, page='1'):
     page_index = get_page_index(page)
-    num = await User.findNumber('count(id)')
+    num = yield from User.findNumber('count(id)')
     p = Page(num, page_index)
     if num == 0:
         return dict(page=p, users=())
-    users = await User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    users = yield from User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
     for u in users:
-        u.password = '******'
+        u.passwd = '******'
     return dict(page=p, users=users)
 
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 @post('/api/users')
-async def api_register_user(*, email, name, passwd):
+def api_register_user(*, email, name, passwd):
     if not name or not name.strip():
         raise APIValueError('name')
     if not email or not _RE_EMAIL.match(email):
         raise APIValueError('email')
     if not passwd or not _RE_SHA1.match(passwd):
         raise APIValueError('passwd')
-    users = await User.findAll('email=?', [email])
+    users = yield from User.findAll('email=?', [email])
     if len(users) > 0:
         raise APIError('register:failed', 'email', 'Email is already in use.')
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid, passwd)
     user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
-    await user.save()
+    yield from user.save()
     # make session cookie:
     r = web.Response()
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
-    user.password = '******'
+    user.passwd = '******'
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
 
 @get('/api/blogs')
-async def api_blogs(*, page='1'):
+def api_blogs(*, page='1'):
     page_index = get_page_index(page)
-    num = await Blog.findNumber('count(id)')
+    num = yield from Blog.findNumber('count(id)')
     p = Page(num, page_index)
     if num == 0:
         return dict(page=p, blogs=())
-    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    blogs = yield from Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
     return dict(page=p, blogs=blogs)
 
 @get('/api/blogs/{id}')
-async def api_get_blog(*, id):
-    blog = await Blog.find(id)
+def api_get_blog(*, id):
+    blog = yield from Blog.find(id)
     return blog
 
 @post('/api/blogs')
-async def api_create_blog(request, *, name, summary, content):
+def api_create_blog(request, *, name, summary, content):
     check_admin(request)
     if not name or not name.strip():
         raise APIValueError('name', 'name cannot be empty.')
@@ -285,13 +286,13 @@ async def api_create_blog(request, *, name, summary, content):
     if not content or not content.strip():
         raise APIValueError('content', 'content cannot be empty.')
     blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip())
-    await blog.save()
+    yield from blog.save()
     return blog
 
 @post('/api/blogs/{id}')
-async def api_update_blog(id, request, *, name, summary, content):
+def api_update_blog(id, request, *, name, summary, content):
     check_admin(request)
-    blog = await Blog.find(id)
+    blog = yield from Blog.find(id)
     if not name or not name.strip():
         raise APIValueError('name', 'name cannot be empty.')
     if not summary or not summary.strip():
@@ -301,12 +302,12 @@ async def api_update_blog(id, request, *, name, summary, content):
     blog.name = name.strip()
     blog.summary = summary.strip()
     blog.content = content.strip()
-    await blog.update()
+    yield from blog.update()
     return blog
 
 @post('/api/blogs/{id}/delete')
-async def api_delete_blog(request, *, id):
+def api_delete_blog(request, *, id):
     check_admin(request)
-    blog = await Blog.find(id)
-    await blog.remove()
+    blog = yield from Blog.find(id)
+    yield from blog.remove()
     return dict(id=id)
